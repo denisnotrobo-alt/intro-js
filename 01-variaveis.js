a36eea6fd46ae7a273ed8c0abe4e99c759fcd099
Mago da Silva <denisnotrobo@gmail.com>
2026-07-06 22:17:30 -0300

    Cria arquivo sobre váriaveis

---
+++ 01-variaveis.js
@@ -0,0 +1,8 @@
+// Comentário no javascript
+// Variavéis são um espaço onde o programa guarda uma informação para poder usar depois
+
+// No javascript, temos 3 formas de declarar uma variável
+
+// 1 - const 
+const nome = "Mago"
+console.log(nome)
